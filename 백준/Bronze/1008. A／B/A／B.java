512ae7e36908b4a3580c1bbc219dd5a036253d94
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-10-08 04:19:10 +0900

    [Bronze V] Title: A/B, Time: 132 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int a = System.in.read() - '0';
+        System.in.read();
+        int b = System.in.read() - '0';
+        System.out.println(String.format("%.9f", (double) a / b));
+    }
+}
