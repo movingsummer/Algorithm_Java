160cf0a0d6146b7c31b548e8eff932ef1d3c87f7
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-10-15 16:15:18 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String result = "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|";
+        System.out.println(result);
+    }
+}
