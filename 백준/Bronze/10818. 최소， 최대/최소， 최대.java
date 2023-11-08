2aff752788f3b3143675c22c9eec675e6a3a5a75
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-11-08 10:47:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 1684 ms, Memory: 310868 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int mn = Integer.MAX_VALUE;
+        int mx = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int tmp = sc.nextInt();
+
+            if (mn > tmp) mn = tmp;
+            if (mx < tmp) mx = tmp;
+        }
+
+        System.out.printf("%d %d", mn, mx);
+    }
+}
