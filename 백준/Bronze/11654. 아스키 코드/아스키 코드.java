9483e321f4dec902af49b37e4c3cdc846e8eec15
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-11-20 03:06:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String c = sc.nextLine();
+
+        System.out.println((int) c.charAt(0));
+
+    }
+}
