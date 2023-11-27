32ce638eba968fa3a545cae98409a01c6e28c948
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-11-27 22:10:05 +0900

    [Bronze V] Title: 새싹, Time: 132 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String str = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.println(str);
+    }
+}
