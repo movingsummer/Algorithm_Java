ed721477d7547e608c9be1ee544f0811297556c2
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-12-08 01:45:45 +0900

    [Bronze V] Title: 검증수, Time: 212 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int result = (a * a + b * b + c * c + d * d + e * e) % 10;
+
+        System.out.println(result);
+    }
+}
