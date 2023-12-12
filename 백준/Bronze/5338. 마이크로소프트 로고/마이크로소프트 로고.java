aad4df812d847f169a83f05f36f5262bb08e0c0f
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-12-13 02:37:40 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
