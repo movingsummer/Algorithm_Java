75faee57c4d5283d72a5ccccf63fe5cfdb9d3c75
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-12-18 03:32:04 +0900

    [Bronze V] Title: 연세대학교, Time: 212 ms, Memory: 17492 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n == 0 ? "YONSEI" : "Leading the Way to the Future");
+    }
+}
