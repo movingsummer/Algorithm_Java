9fc43b1828e24f4fef8027fe210f97afefce3f7b
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-12-21 03:11:52 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 216 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int ax = sc.nextInt();
+        int ay = sc.nextInt();
+        int az = sc.nextInt();
+        int cx = sc.nextInt();
+        int cy = sc.nextInt();
+        int cz = sc.nextInt();
+
+        System.out.printf("%d %d %d", cx - az, cy / ay, cz - ax);
+    }
+}
