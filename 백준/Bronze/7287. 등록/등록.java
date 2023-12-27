a55f106683cfbaa2442ae182ebc24cc47d1c2bb1
movingsummer <111179843+movingsummer@users.noreply.github.com>
2023-12-28 05:56:12 +0900

    [Bronze V] Title: 등록, Time: 112 ms, Memory: 13816 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(83 + "\n" + "movingsummer");
+    }
+}
